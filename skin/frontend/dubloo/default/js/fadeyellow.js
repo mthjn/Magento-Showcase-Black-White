ad26507d3aa176e883b4c1ca35cebedcdae0e393
mthjn <jana.mathauserova@ubk.cz>
2015-05-20 15:19:47 +0200

    footer and sidebar

--- skin/frontend/dubloo/default/js/fadeyellow.js
+++ skin/frontend/dubloo/default/js/fadeyellow.js
@@ -1,10 +1,19 @@
 var msg      = document.getElementById("yellowmsg"),
     cross   = document.getElementById("closeyellow");
-    console.log(msg + '  ' + cross);
+    //console.log(msg + '  ' + cross);
+
+if (document.cookie.indexOf("closedmessage") >= 0) {
+  console.log("hello again");
+  msg.className = 'disabled';
+}
 
 window.addEventListener("load", function(){
   cross.onclick = function(e) {
         msg.className = 'disabled';
-        // add cookie
+
+        var expiry = new Date();
+        expiry.setDate(expiry.getDate() + 60); // 1 Min
+        document.cookie = "closedmessage=yes; expires=" + expiry.toUTCString();
+        console.log("setting the cookie");
     }
 });
